1ca6dbf551c3c5bcd664b7efb6407af944a836b4
unknown <sharonkessio6@gmail.com>
2022-11-10 16:50:06 +0300

    created filter enumarables

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -9,4 +9,10 @@ module MyEnumerable
     each { |element| result = true if yield element }
     result
   end
+  def filter?
+    result = []
+    each { |element| result << element if yield element }
+    result
+  end
+
 end
--- mylist.rb
+++ mylist.rb
@@ -19,3 +19,5 @@ puts list.all? { |e| e > 5 }
 
 puts list.any? {|e| e == 2}
 puts list.any? {|e| e == 5}
+
+puts list.filter? {|e| e.even?}

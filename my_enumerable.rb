4d80786af95633f71dee5305c56511dfadc56f5f
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-11-10 16:38:30 +0300

    Solve for any?

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -4,4 +4,9 @@ module MyEnumerable
     each { |element| result = false unless yield element }
     result
   end
+  def any?
+    result = false
+    each { |element| result = true if yield element }
+    result
+  end
 end
--- mylist.rb
+++ mylist.rb
@@ -16,3 +16,6 @@ list = MyList.new(1, 2, 3, 4)
 puts list
 puts list.all? { |e| e < 5 }
 puts list.all? { |e| e > 5 }
+
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
